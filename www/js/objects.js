befc2fb266823209d4a2b94fe00335e36cee9c44
Estrella Basurto <ebasurto002@ehu.es>
2017-11-13 13:57:22 +0000

    No quería hacer este commit de mierda, pero se me ha puesto tonto el amigo. Sólo había añadido mi IP a las urls del servidor. Pero vamos, que no va a ningún sitio

--- www/js/objects.js
+++ www/js/objects.js
@@ -67,7 +67,8 @@ var appConstants = {
 //	uploadFileURL: "http://192.168.0.38:8080/TTA1718_LS-EX_09-10S/rest/School/uploadFile" //HOME
 	//serverURL : "http://192.168.0.19:8080/GraphiAppServer/",
 	//serverURL : "http://192.168.0.18:8080/GraphiAppServer/",
-	serverURL : "http://10.109.179.85:8080/GraphiAppServer/",
+	//serverURL : "http://10.109.179.85:8080/GraphiAppServer/",
+	serverURL : "http://192.168.0.16:8080/GraphiAppServer/",
 	get ejerciciosNivel1URL () {
 	    return this.serverURL + "rest/GraphiApp/getNivel1";
 	},
