928dbb25023944b60922178af0197c9ff90dc97f
Jon Echebarria <jechebarria008@ikasle.ehu.eus>
2017-11-14 11:44:24 +0100

    Cambiadas referencias a sessionConstants en nivel1_start y nivel2_start. Ahora el objeto a utilizar es studentSessionConstants

--- www/js/functions.js
+++ www/js/functions.js
@@ -5,7 +5,7 @@
 			nivel1.index=0;
 			nivel1.total=0;
 			nivel1.correctas=0;
-			$.getJSON(appConstants.ejerciciosNivel1URL,{nickname: sessionConstants.nickname,pin: pin},function( data ) {
+			$.getJSON(appConstants.ejerciciosNivel1URL,{nickname: studentSessionConstants.nickname,pin: pin},function( data ) {
 				nivel1.palabras=data.nivel1;
 				$("#nivel1-palabra-1").html(nivel1.palabras[0].palabra1);
 				$("#nivel1-palabra-2").html(nivel1.palabras[0].palabra2);
@@ -45,7 +45,7 @@
 			nivel2.total=0;
 			nivel2.correctas=0;
 			$("#nivel2-letras").empty();
-			$.getJSON(appConstants.ejerciciosNivel2URL,{nickname: sessionConstants.nickname,pin: pin},function( data ) {
+			$.getJSON(appConstants.ejerciciosNivel2URL,{nickname: studentSessionConstants.nickname,pin: pin},function( data ) {
 				nivel2.palabras=data.nivel2;
 				var index=0;
 				nivel2.palabras[0].palabra.split('').forEach(function(letra) {
